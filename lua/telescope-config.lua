be2f7c055d86f07ac1b8d5019835932d0456eb96
keatmin <keatmin3@gmail.com>
2022-11-27 18:55:04 +1100

    [ktmn] Overwrite fzf mappings

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -17,7 +17,8 @@ require('telescope').load_extension('fzf')
 -- Telescope keymap 
 vim.keymap.set('n', '<leader>ff', builtin.find_files)
 vim.keymap.set('n', '<leader>fg', builtin.live_grep)
-vim.keymap.set('n', '<leader>fb', builtin.buffers)
+vim.keymap.set('n', '<leader><tab>', builtin.buffers)
 vim.keymap.set('n', '<leader>fh', builtin.help_tags)
 vim.keymap.set('n', '<leader>fh', builtin.current_buffer_fuzzy_find)
+vim.keymap.set('n', '<leader>fd',builtin.diagnostics)
 
