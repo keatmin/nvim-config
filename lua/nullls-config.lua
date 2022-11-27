43079636ed361eb93ada24a38972e8de1a0394cd
keatmin <keatmin.woo@shippit.com>
2022-11-28 01:51:04 +1100

    [ktmn] Update flake8 diagnostics

--- lua/nullls-config.lua
+++ lua/nullls-config.lua
@@ -3,7 +3,9 @@ local lsp_format = require("lsp-format")
 
 -- register any number of sources simultaneously
 local sources = {
-    null_ls.builtins.diagnostics.flake8,
+    null_ls.builtins.diagnostics.flake8.with({
+        extra_args = {"--max-line-length", 88}
+    }),
     null_ls.builtins.diagnostics.pylint,
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.reorder_python_imports,
