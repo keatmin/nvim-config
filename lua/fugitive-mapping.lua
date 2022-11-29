8c0b188318cd5dc9baf9b2319de3028832ca6b58
keatmin <keatmin3@gmail.com>
2022-11-30 00:58:59 +1100

    [ktmn] Add more fugitive-mapping

--- lua/fugitive-mapping.lua
+++ lua/fugitive-mapping.lua
@@ -1,3 +1,6 @@
+local opts = {silent=true}
 vim.keymap.set('n', '<leader>gd',':Gdiffsplit<CR>')
-vim.keymap.set('n', '<leader>gh',':G<CR>')
-
+vim.keymap.set('n', '<leader>gg',':G<CR>',opts)
+vim.keymap.set('n', '<leader>gb',':G blame<CR>',opts)
+vim.keymap.set('n', '<leader>gp',':G push')
+vim.keymap.set('n', '<leader>gl',':Gclog<CR>',opts)
