a1efcf8e63d7d0f0ca2c62d5f10c4e0546fc64b1
keatmin <keatmin.woo@shippit.com>
2023-04-19 16:51:42 +1000

    [ktmn] feat(fugitive): add Git toggle mapping

--- lua/fugitive-mapping.lua
+++ lua/fugitive-mapping.lua
@@ -1,6 +1,27 @@
 local opts = {silent=true}
 vim.keymap.set('n', '<leader>gd',':Gdiffsplit<CR>')
-vim.keymap.set('n', '<leader>gg',':G<CR>',opts)
 vim.keymap.set('n', '<leader>gb',':G blame<CR>',opts)
 vim.keymap.set('n', '<leader>gp',':G push')
 vim.keymap.set('n', '<leader>gl',':Gclog<CR>',opts)
+
+local function showFugitiveGit()
+  if vim.fn.FugitiveHead() ~= '' then
+    vim.cmd [[
+    Git
+    " wincmd H  " Open Git window in vertical split
+    " setlocal winfixwidth
+    " vertical resize 31
+    " setlocal winfixwidth
+    setlocal nonumber
+    setlocal norelativenumber
+    ]]
+  end
+end
+local function toggleFugitiveGit()
+  if vim.fn.buflisted(vim.fn.bufname('fugitive:///*/.git//$')) ~= 0 then
+    vim.cmd[[ execute ":bdelete" bufname('fugitive:///*/.git//$') ]]
+  else
+    showFugitiveGit()
+  end
+end
+vim.keymap.set('n', '<leader>gg', toggleFugitiveGit, opts)
