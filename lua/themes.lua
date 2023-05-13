93b07e9ce6ee69c39d7a7a0072b167f42797d928
keatmin <keatmin.woo@shippit.com>
2023-05-13 23:58:17 +1000

    [ktmn] style(themes): use catppuccin

--- lua/themes.lua
+++ lua/themes.lua
@@ -1,2 +1,2 @@
-vim.cmd("colorscheme tokyonight-moon")
-vim.cmd('let g:lightline = {"colorscheme": "tokyonight-moon"}')
+vim.cmd("colorscheme catppuccin-mocha")
+vim.cmd('let g:lightline = {"colorscheme": "catppuccin"}')
