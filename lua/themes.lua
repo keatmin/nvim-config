c5833d3db9e30ad2b23f64b7db97da76b026c963
keatmin <keatmin3@gmail.com>
2024-05-04 18:10:15 +1000

    [ktmn] add fugitive to lightline

--- lua/themes.lua
+++ lua/themes.lua
@@ -1,2 +1,13 @@
 vim.cmd("colorscheme catppuccin-mocha")
-vim.cmd('let g:lightline = {"colorscheme": "catppuccin"}')
+vim.cmd([[let g:lightline = {
+    \ "colorscheme": "catppuccin",
+    \ "active": {
+    \   "left": [ [ "mode", "paste" ],
+    \             [ "gitbranch", "readonly", "filename", "modified" ] ]
+    \ },
+    \ "component_function": {
+    \   "gitbranch": "FugitiveHead"
+    \ },
+    \ }
+]])
+
