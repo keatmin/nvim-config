0d14b82d86fb9c5f55c66a3f870c0d908f54fcd7
keatmin <keatmin3@gmail.com>
2024-05-15 00:28:44 +1000

    [ktmn] update fzf-lua

--- lua/fzf-lua-config.lua
+++ lua/fzf-lua-config.lua
@@ -2,27 +2,31 @@ local fzf = require("fzf-lua")
 fzf.setup({
 	"telescope",
 	winopts = {
-		preview = { delay = 0 },
+		preview = {
+			delay = 0,
+		},
 		builtin = {
 			treesitter = { disable = true, enable = {} },
 		},
 	},
+	header = false,
 })
 
 vim.keymap.set(
 	"n",
 	"<leader>ff",
-	"<cmd>lua require('fzf-lua').files({ prompt='Files> ', cwd_prompt=false})<CR>",
+	"<cmd>lua require('fzf-lua').files({ prompt='Files> ', cwd_prompt=false, header=false})<CR>",
 	{ silent = true }
 )
 vim.keymap.set(
 	"n",
 	"<leader><CR>",
-	"<cmd>lua require('fzf-lua').files({ prompt='Projects> ', cwd = '~/Projects', cwd_prompt=false })<CR>",
+	"<cmd>lua require('fzf-lua').files({ prompt='Projects> ', cwd = '~/Projects', cwd_prompt=false, header=false })<CR>",
 	{ silent = true }
 )
-vim.keymap.set("n", "<leader>fg", "<cmd>lua require('fzf-lua').live_grep_glob()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fg", "<cmd>lua require('fzf-lua').live_grep_glob({header=false})<CR>", { silent = true })
 vim.keymap.set("n", "<leader>fc", "<cmd>lua require('fzf-lua').grep_cword()<CR>", { silent = true })
-vim.keymap.set("n", "<leader><tab>", "<cmd>lua require('fzf-lua').buffers()<CR>", { silent = true })
+vim.keymap.set("n", "<leader><tab>", "<cmd>lua require('fzf-lua').buffers({header=false})<CR>", { silent = true })
 vim.keymap.set("n", "<leader>fb", "<cmd>lua require('fzf-lua').blines()<CR>", { silent = true })
-vim.keymap.set("n", "<leader><leader>", "<cmd>lua require('fzf-lua').marks()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fm", "<cmd>lua require('fzf-lua').marks()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fm", "<cmd>lua require('fzf-lua').marks()<CR>", { silent = true })
